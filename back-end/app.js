fec37ad349a52fd4e76fd5fd4e94781fc59b68d1
Amel252 <amelghanem25@gmail.com>
2025-10-22 16:02:47 +0200

    first commit

---
+++ back-end/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "back-end/config/config.env" });
+
+const app = express();
+app.listen(process.env.PORT, () => {
+    console.log(`Le serveur est lancé sur le port: ${process.env.PORT}`);
+});
