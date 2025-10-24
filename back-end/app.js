6abcf74feb5156b09d719819cda94281a3ebd5b7
Amel252 <amelghanem25@gmail.com>
2025-10-24 14:58:22 +0200

    Connexion à la BD mongoDB

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config({ path: "back-end/config/config.env" });
 import ProductRoutes from "./routes/productRoute.js";
+import { connectedDatabase } from "./config/dbConnect.js";
 
 const app = express();
 
+connectedDatabase();
 app.use("/api/v1", ProductRoutes);
 
 app.listen(process.env.PORT, () => {
---
+++ back-end/config/dbConnect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectedDatabase = () => {
+    let DB_URI = "";
+    if (process.env.NODE_ENV === "DEVELOPMENT")
+        DB_URI = process.env.DB_URI_LOCAL;
+    if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+    mongoose.connect(DB_URI).then((con) => {
+        console.log(`connexion avec le host:${con?.connection?.host}`);
+    });
+};
