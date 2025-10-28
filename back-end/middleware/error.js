fb433499cd97dde86eeccee4fb1226f3b1680f37
Amel252 <amelghanem25@gmail.com>
2025-10-28 16:18:50 +0100

    Gestion des erreurs pour mongoose

--- back-end/middleware/error.js
+++ back-end/middleware/error.js
@@ -6,8 +6,30 @@ export default (err, req, res, next) => {
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal server",
     };
-    // si je suis en devlopement
+    // erreur venant de mongoose
+    if (error.name === "CastError") {
+        const message = `Ressource not found.invalid:${err?.path}`;
+        error = new ErrorHandler(message, 404);
+    }
+    if (error.name === "ValidationError") {
+        const message = Object.values(err.errors).map((value) => value.message);
+        error = new ErrorHandler(message, 400);
+    }
+    if (err.code === 1000) {
+        const message = `Duplicate${Object.keys(err.keyValue)} entered`;
+        error = new ErrorHandler(message, 400);
+    }
+    if (error.name === "JsonWebTokenError") {
+        const message = `JSON Web Token is invalid, try Again !!`;
+        error = new ErrorHandler(message, 400);
+    }
+    if (error.name === "TokenExpiredError") {
+        const message = `JSON Web Token is expire , try Again !!`;
+        error = new ErrorHandler(message, 400);
+    }
+    // Erreur de dev ou prod
     if (process.env.NODE_ENV === "DEVELOPMENT") {
+        // si je suis en devlopement
         res.status(error.statusCode).json({
             message: error.message,
             error: err,
