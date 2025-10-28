234bcb6dec1a7c4632cea71f4aa12c0eb0df2b02
Amel252 <amelghanem25@gmail.com>
2025-10-28 15:53:32 +0100

    Gestion d'erreur en dev et en prod

--- back-end/middleware/error.js
+++ back-end/middleware/error.js
@@ -1,11 +1,26 @@
 import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
-    // les erreurs contenant le statusCode
+    // les erreurs contenant le statusCode server
     let error = {
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal server",
     };
+    // si je suis en devlopement
+    if (process.env.NODE_ENV === "DEVELOPMENT") {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err?.stack,
+        });
+    }
+    // si je suis en prod
+    if (process.env.NODE_ENV === "PRODUCTION") {
+        res.status(error.statusCode).json({
+            message: error.message,
+        });
+    }
+
     res.status(error.statusCode).json({
         message: error.message,
     });
