84b7ca59052dd9cdf48c5a77a9bf2aaa8248251b
Amel252 <amelghanem25@gmail.com>
2025-11-10 16:12:13 +0100

    Création de functions qui permet la récupération toute les  orders et orderDetails

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -31,3 +31,24 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
         order,
     });
 });
+//  get Order details => /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+    // populate permet de referencier l'user
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this ID", 404));
+    }
+    res.status(200).json({
+        order,
+    });
+});
+//  get current user's orders => /api/v1/me/orders/
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json({
+        orders,
+    });
+});
--- back-end/routes/orderRoute.js
+++ back-end/routes/orderRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { isAuthentificatedUser } from "../middleware/auth.js";
-import { newOrder } from "../controllers/orderController.js";
+import {
+    getOrderDetails,
+    myOrders,
+    newOrder,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 router.route("/orders/new").post(isAuthentificatedUser, newOrder);
+router.route("/orders/:id").get(isAuthentificatedUser, getOrderDetails);
+router.route("/me/orders").get(isAuthentificatedUser, myOrders);
 
 export default router;
