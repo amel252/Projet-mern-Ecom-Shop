acd09305d1fd902f318c0f61c8b2710bf363b159
Amel252 <amelghanem25@gmail.com>
2025-11-15 14:47:01 +0100

    installation de react avec (create-react-app)

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-center bg-success p-y-2">Un super site </h1>
+        </div>
+    );
+}
+
+export default App;
