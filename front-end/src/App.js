a3fa6f5eec377d4c8102ff26bdbb9a7a51eb0671
Amel252 <amelghanem25@gmail.com>
2025-11-17 15:43:40 +0100

    installation react-router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
+//  import
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//  importer les composants
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
